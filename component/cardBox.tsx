ad59dd89d6c1ef4ddb1de693dfa20708f067154d
bonteves43 <bonteves43@gmail.com>
2025-11-25 22:23:39 +0800

    created box with the header

---
+++ component/cardBox.tsx
@@ -0,0 +1,19 @@
+export default function CardBox() {
+  return (
+    // Main Div
+    <div className="bg-red-500 w-full rounded-xl max-w-md m-10 overflow-hidden">
+      {/* Header Div  */}
+      <div className="bg-blue-700 p-4 flex items-center gap-3">
+        <div className="flex bg-red-800 h-9 w-16 rounded-md shadow-sm items-center justify-center font-bold text-xl ">
+          62C
+        </div>
+        <div className="bg-blue-500 w-lg h-10 rounded-2xl text-center p-2 font-bold ">
+          TALAMBAN PITOS CARBON VIA ECHAVEZ
+        </div>
+      </div>
+
+      <div className="bg-amber-500 w-1/2 ">hehuh</div>
+      <div className="bg-green-500 w-1/2">hihi </div>
+    </div>
+  );
+}
