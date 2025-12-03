ef866a698a21e94ea1bbc989e01134aab304b680
BON JOSHUA TEVES <149884375+konnichiwabon@users.noreply.github.com>
2025-12-03 07:00:01 +0000

    create a map repo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import CardBox from "@/component/cardBox";
+import Map from "@/component/map";
 
 export default function Home() {
-  return <CardBox />;
+  return (
+  <div>
+    <div>
+      <Map />
+      <div>
+        <CardBox/>
+      </div>
+    </div>
+    
+    
+    
+    
+  </div>)
+        
 }
