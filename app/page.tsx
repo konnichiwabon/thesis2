e6f83dfdfd5bad80656231b851546a00213d3c48
BON JOSHUA TEVES <149884375+konnichiwabon@users.noreply.github.com>
2025-12-04 05:46:06 +0000

    created a bg map w/ errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import CardBox from "@/component/cardBox";
-import Map from "@/component/map";
+import MapComponent from "@/component/map";
 
 export default function Home() {
   return (
   <div>
     <div>
-      <Map />
+      import MapComponent from './MapComponent';
+
+// Inside your main component:
+<MapComponent coords={[10.335682729765237, 123.9112697095374]} />
       <div>
         <CardBox/>
       </div>
--- component/map.tsx
+++ component/map.tsx
@@ -1,6 +1,49 @@
-export default function Map(){
+"use client"
 
-    return(
-        <div></div>
-    )
-}
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// --- Fix for default Leaflet marker icons in React ---
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+// -----------------------------------------------------
+
+const MapComponent = ({ coords }) => {
+  // Default to Cebu City if no coords are provided
+  const centerPosition = coords || [10.3157, 123.8854];
+
+  return (
+    <div style={{ height: '100vh', width: '100%', position: 'absolute', top: 0, left: 0, zIndex: 0 }}>
+      <MapContainer 
+        center={centerPosition} 
+        zoom={15} 
+        scrollWheelZoom={true} 
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        
+        <Marker position={centerPosition}>
+          <Popup>
+            Vehicle Location
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default MapComponent;
